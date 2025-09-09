13370936119a19738b6e37c180bb6390b1187b27
atlasecom <your-github-email@example.com>
2025-09-09 04:23:32 +0100

    fix: Remove black background from navigation menu only on mobile design while keeping it on desktop

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = ({ active }) => {
     const { i18n } = useTranslation();
 
     return (
-        <div className={`block 800px:${styles.noramlFlex} pr-12 bg-black rounded-lg p-4`}>
+        <div className={`block 800px:${styles.noramlFlex} pr-12 800px:bg-black rounded-lg p-4`}>
             {
                 navItems.map((i, index) => (
                     <div className='flex' key={index}>
                         <Link to={i.url}
-                            className={`${active === index + 1 ? "text-orange-500 font-bold" : "text-white hover:text-gray-300"} pb-[30px] 800px:pb-0 font-[500] px-6 cursor-pointer transition-colors duration-200`}
+                            className={`${active === index + 1 ? "text-orange-500 font-bold" : "text-gray-800 800px:text-white hover:text-gray-600 800px:hover:text-gray-300"} pb-[30px] 800px:pb-0 font-[500] px-6 cursor-pointer transition-colors duration-200`}
                         >
                             {i.title[i18n.language] || i.title.en}
                         </Link>
